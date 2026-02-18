a1334784185caf263c181252e1b2cbf3a7ba8f51
dev-brunosantos <brunosdesenvolvedor@gmail.com>
2026-02-17 23:42:14 -0300

    Criando documentação da API com Swagger

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,5 +1,7 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./configs/swagger.js";
 import { routes } from "./routes/routes.js";
 import { LoginController } from "./controllers/LoginController.js";
 
@@ -12,9 +14,34 @@ app.use(routes.userRoutes);
 
 const loginController = new LoginController();
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Autenticação do usuário
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/LoginRequest'
+ *     responses:
+ *       200:
+ *         description: Login realizado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/LoginResponse'
+ *       401:
+ *         description: Credenciais inválidas
+ */
+
 app.post('/login', async (req: Request, res: Response) => {
     await loginController.login(req, res);
 })
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 export { app };
